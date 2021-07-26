dbb85ee8de80cfdf5bb5c4076adc269569c76882
rishubm <rishubmadhav@gmail.com>
2021-07-26 16:26:35 -0500

    Chatbot can now parse user feedback using sentiment analysis and respond with a custom action.
    Created intent feedback and action_feedback

--- actions/actions.py
+++ actions/actions.py
@@ -41,3 +41,23 @@ class ActionHelloWorld(Action):
         dispatcher.utter_message(response)
 
         return [SlotSet('GPE',city)]
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_feedback"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        ent = tracker.get_latest_entity_values("sentiment", None)
+        response = "Thank you for letting me know."
+        if ent[0] == "pos":
+            response = "I'm glad you thought so. Goodbye."
+        elif ent[0] == "neg":
+            response = "I'm sorry you had that experience."
+        
+        print(ent)
+        dispatcher.utter_message(response)
+
+        return []
